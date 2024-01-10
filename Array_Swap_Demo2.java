c47518034ba89ca22d32bee969f2fbf592277b02
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-10 23:10:04 +0530

    Add files via upload

---
+++ Array_Swap_Demo2.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class Array_Swap_Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 10, 20, 5, 7, 8 };
+		System.out.println(arr[0] + " " + arr[1]);// 10 20
+		Swap(arr, 0, 1);
+		System.out.println(arr[0] + " " + arr[1]);//
+	}
+
+	public static void Swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+
+	}
+
+}
